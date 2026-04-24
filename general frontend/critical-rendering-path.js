a603aec362111b3189af53446320e9334e67cb67
Shubham Rohilla <srohilla099@gmail.com>
2026-04-24 17:43:05 +0530

    initial commit

---
+++ general frontend/critical-rendering-path.js
@@ -0,0 +1,3 @@
+// If asked, say this:
+
+// The Critical Rendering Path is the process the browser uses to render a webpage, starting from parsing HTML and CSS to building the render tree and painting pixels. Optimizing CRP involves reducing render-blocking resources, prioritizing critical content, and deferring non-essential assets to improve initial load performance.
